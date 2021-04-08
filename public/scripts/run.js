e8470290ed7ebaf35061bd838fff5d282482b433
Tyler BoddySpargo <muyleche@gmail.com>
2021-04-07 19:04:18 -0500

    fix(routing): fix scrolling on route change to hash

--- public/scripts/run.js
+++ public/scripts/run.js
@@ -1,24 +1,40 @@
 define([
-    'scripts/app',
+  'scripts/app',
 ],
-function(ngApp, FirebaseApp) {
+  function (ngApp, FirebaseApp) {
 
     // initiate app and handle route changes that also scroll to location.
-    ngApp.run(['$rootScope', '$location', '$anchorScroll', '$routeParams', '$timeout', function ($rootScope, $location, $anchorScroll, $routeParams, $timeout) {
-        $anchorScroll.yOffset = 50; // always scroll by 50 extra pixels
-        $rootScope.images = [];
-        $rootScope.activeImage = 0;
+    ngApp.run(['$rootScope', '$location', '$anchorScroll', '$timeout', function ($rootScope, $location, $anchorScroll, $timeout) {
+      $anchorScroll.yOffset = 60; // always scroll by 50 extra pixels
+      $rootScope.images = [];
+      $rootScope.activeImage = 0;
 
-        $rootScope.$on('$routeChangeSuccess',
-            function (next, current) {
-                var loc = $routeParams.loc;
-                if (loc) {
-                    $timeout(function () {
-                        $location.url($location.path());
-                        $location.hash(loc);
-                        $anchorScroll();
-                    }, null, null, loc);
-                }
-            });
-    } ]);
-});
+      let previousAnchor = '';
+      $rootScope.scrollTo = (anchor = $location.hash(), delay = 0, trigger = 'unknown') => {
+        $timeout(() => {
+          if (anchor != previousAnchor) {
+            console.debug(`${trigger} scrolling to ${anchor} from ${previousAnchor} after waiting ${delay}ms`);
+            $anchorScroll(anchor);
+            previousAnchor = anchor;
+          }
+        }, delay, false);
+      };
+
+      function locationChangeHandler(event) {
+        const currentHash = $location.hash();
+        let delay;
+        switch (event.targetScope.name) {
+          case 'about':
+            delay = 200;
+            break;
+          case 'projects':
+            delay = 200;
+            break;
+        }
+        $rootScope.scrollTo(currentHash, delay, event.name);
+      }
+
+      $rootScope.$on('$viewContentLoaded', locationChangeHandler);
+
+    }]);
+  });
