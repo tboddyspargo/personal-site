3755fd6713af478da350cdb1a8cd9ed27f0c2679
Tyler BoddySpargo <muyleche@gmail.com>
2021-04-12 16:46:38 -0500

    feat(loading): update directive with isolated scope attributes

--- public/scripts/directives/progress_bar.js
+++ public/scripts/directives/progress_bar.js
@@ -1,24 +1,22 @@
-define(['scripts/app', 'utils/scroll'],
-  function (ngApp, scroll) {
+define(['scripts/app'],
+  function (ngApp) {
     // directive that updates progress bar based on given scope value to watch.
-    ngApp.directive('progressBar', function () {
-      return {
-        restrict: 'A',
-        link: (scope, element, attrs) => {
-          var watchFor = attrs.progressBarWatch;
-          var total = attrs.ariaValuemax;
-          // update now
-          var val = scope[watchFor];
-          element.attr('aria-valuenow', val);
-          element.css('width', (val / total * 100) + "%");
-
-          // watch for the value
-          scope.$watch(watchFor, function (val) {
-            element.attr('aria-valuenow', val);
-            element.css('width', (Math.round(val / total * 100)) + "%");
-            if (val / total === 1 || scope[watchFor] === undefined) { element.parent().parent().css('display', 'none'); }
-          });
-        }
-      };
-    });
+    ngApp.directive('tbsProgress', () => ({
+      restrict: 'E',
+      replace: true,
+      scope: {
+        progressValue: '@value',
+        progressMax: '@max'
+      },
+      templateUrl: '/templates/loading.html',
+      link: (scope, element, attrs) => {
+        scope.in_progress = true
+        scope.$watch(
+          () => scope.progressValue / scope.progressMax,
+          (newValue, oldValue) => {
+            scope.in_progress = newValue < 1;
+          }
+        );
+      }
+    }));
   });
