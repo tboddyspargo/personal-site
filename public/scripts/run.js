e5781fff28dfcf8140a8b6c2138c3ce426de8d4a
Tyler BoddySpargo <muyleche@gmail.com>
2021-04-12 16:48:20 -0500

    feat(views): add loading directive to each view

--- public/scripts/run.js
+++ public/scripts/run.js
@@ -3,6 +3,7 @@ define(['scripts/app'],
     // initiate app and handle route changes that also scroll to location.
     ngApp.run(['$rootScope', '$location', '$anchorScroll', '$timeout', function ($rootScope, $location, $anchorScroll, $timeout) {
       $anchorScroll.yOffset = 60; // always scroll by 50 extra pixels
+
       let previousAnchor = '';
       $rootScope.scrollTo = (anchor = $location.hash(), delay = 0, trigger = 'unknown') => {
         $timeout(() => {
