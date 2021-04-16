581cddea8f896d9922cd06cac00f2804beafaa63
Tyler BoddySpargo <muyleche@gmail.com>
2021-04-16 11:02:10 -0500

    feat(scroll): write as true ng service

--- public/scripts/directives/banner.js
+++ public/scripts/directives/banner.js
@@ -1,17 +1,16 @@
-define(['scripts/app', 'utils/scroll'],
-  function (ngApp, scroll) {
-    function bannerController() {
-      scroll.configureBannerParallax();
-      // TODO: add fade to black.
+define(['scripts/app'], (ngApp) => {
+  function bannerController(ScrollService) {
+    ScrollService.configureBannerParallax();
+    // TODO: add fade to black.
+  };
+
+
+  // banner as element
+  ngApp.directive('tbsBanner', function () {
+    return {
+      restrict: 'E',
+      templateUrl: '/templates/banner.html',
+      controller: ['ScrollService', bannerController],
     };
-
-
-    // banner as element
-    ngApp.directive('tbsBanner', function () {
-      return {
-        restrict: 'E',
-        templateUrl: '/templates/banner.html',
-        controller: bannerController,
-      };
-    });;
-  });
+  });;
+});
