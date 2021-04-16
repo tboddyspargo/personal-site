075d0854ef39f8fd1fa35e9c4f3c85bedf0b9933
Tyler BoddySpargo <muyleche@gmail.com>
2021-04-15 23:47:16 -0500

    feat(sidebar): implement sidebar as component with isolate scope

--- public/scripts/directives/sidebar.js
+++ public/scripts/directives/sidebar.js
@@ -1,35 +1,47 @@
-define(['scripts/app', 'utils/scroll'],
-  function (ngApp, scroll) {
-    // configuring targets and scroll behavior for the sidebar navigator links.
-    ngApp.directive('tbsSidebar', ['$timeout', ($timeout) => {
-      return {
-        restrict: 'EA',
-        replace: true,
-        scope: {
-          sections: '=',
-          ids: '='
-        },
-        link: (scope, element, attrs) => {
-          scope.$watch(
-            () => scope.ids,
-            (newValue, oldValue) => {
-              if (newValue != oldValue) {
-                $timeout(() => {
-                  element.ready(() => {
-                    if (attrs.hasOwnProperty('ids') && scope.ids) {
-                      scroll.configureSectionNavigationBehavior(scope.ids);
-                    } else if (attrs.hasOwnProperty('sections') && scope.sections) {
-                      scroll.configureSectionNavigationBehavior(scope.sections.map((i) => i.id))
-                    } else {
-                      console.log('No sections or ids were provided to tbs-sidebar.');
-                    }
-                  });
-                }, 100, false);
-              }
-            }
-          );
-        },
-        templateUrl: '/templates/sidebar.html'
+define(['scripts/app', 'utils/scroll'], (ngApp, Scroll) => {
+  // configuring targets and scroll behavior for the sidebar navigator links.
+  ngApp.component('tbsSidebar', {
+    bindings: {
+      sections: '<',
+      // the id reference strings of elements for which there should be links
+      ids: '<'
+    },
+    templateUrl: '/templates/sidebar.html',
+    controller: ['$scope', '$element', '$attrs', '$timeout', function ($scope, $element, $attrs, $timeout) {
+      const ctrl = this;
+
+      /*
+      ** @summary Normalize an array of sections based on either sections or ids given.
+      */
+      ctrl.final_sections = function () {
+        if ($attrs.hasOwnProperty('ids') && Array.isArray(ctrl.ids)) {
+          return ctrl.ids.map(id => ({ id: `${id}`.trimStart('#') }));
+        }
+        if ($attrs.hasOwnProperty('sections') && Array.isArray(ctrl.sections)) {
+          return ctrl.sections.map(({ id, name }) => ({ id: `${id}`.trimStart('#'), name }));
+        }
+        return [];
       };
-    }]);
+
+      /*
+      ** @summary Normalize an array of ids from the available sections.
+      */
+      ctrl.final_ids = () => ctrl.final_sections().map(i => i.id);
+
+      /*
+      ** @summary Configure scrolling behavior. Use $timeout to
+      **   ensure sufficient cycles have completed for elements to have loaded.
+      */
+      function setSidebarScrollBehavior() {
+        $timeout(() => {
+          $timeout(() => {
+            Scroll.configureSectionNavigationBehavior(ctrl.final_ids())
+          });
+        });
+      };
+
+
+      ctrl.$postLink = setSidebarScrollBehavior;
+    }]
   });
+});
