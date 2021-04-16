a79b7453226c07879a4442562f7fe04d4d6623d5
Tyler BoddySpargo <muyleche@gmail.com>
2021-04-15 23:48:03 -0500

    feat(loading): implement progress bar as component

--- public/scripts/directives/progress_bar.js
+++ public/scripts/directives/progress_bar.js
@@ -1,30 +1,48 @@
-define(['scripts/app'],
-  function (ngApp) {
-    // directive that updates progress bar based on given scope value to watch.
-    ngApp.directive('tbsProgress', () => ({
-      restrict: 'E',
-      replace: true,
-      scope: {
-        progressValue: '@value',
-        progressMax: '@max',
-        complete: '@'
-      },
-      templateUrl: '/templates/loading.html',
-      link: (scope, element, attrs) => {
-        scope.isIndeterminate = attrs.$attr.hasOwnProperty('indeterminate');
-        scope.in_progress = true;
-        scope.$watch(
-          () => {
-            if (scope.isIndeterminate) {
-              return scope.complete !== 'true';
-            } else {
-              return (scope.progressValue / (scope.progressMax || 1)) < 1;
-            }
-          },
-          (newValue, oldValue) => {
-            scope.in_progress = newValue;
-          }
-        );
-      }
-    }));
+define(['scripts/app'], (ngApp) => {
+  // Component directive that updates progress bar based on given scope value to watch.
+  ngApp.component('tbsProgress', {
+    bindings: {
+      value: '<',
+      max: '<',
+      description: '@?',
+      indeterminate: '@?'
+    },
+    templateUrl: '/templates/loading.html',
+    controller: ['$scope', '$element', '$attrs', '$timeout', function ($scope, $element, $attrs, $timeout) {
+      const ctrl = this;
+
+      // determine if this progress bar is intended to be indeterminate based on attributes.
+      ctrl.isIndeterminate = $attrs.indeterminate !== undefined;
+
+      /*
+      ** @summary Allows a default of 1 if 'max' isn't provided.
+      ** @param {Number} num    A number (default is ctrl.max).
+      */
+      ctrl.getMax = () => ctrl.max || 1;
+
+      /*
+      ** @summary Returns a number that has been forced to fit within 0 and the current max.
+      ** @param {Number} num = A number.
+      */
+      function clamp(num) {
+        return Math.max(0, Math.min(ctrl.getMax(), num));
+      };
+
+      /*
+      ** @summary Returns the 'value' property that has been clamped between 0 and the current max.
+      */
+      ctrl.getValue = () => {
+        return clamp(ctrl.value);
+      };
+
+      ctrl.inProgress = function () {
+        if (ctrl.isIndeterminate) {
+          // value is expected to be "truthy" if progress is complete.
+          return !ctrl.value;
+        } else {
+          return 1 > (ctrl.getValue() / ctrl.getMax());
+        }
+      };
+    }]
   });
+});
