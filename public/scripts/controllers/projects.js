eb5b8a1df6423cc35cb92a57cfa3ec53c72cad67
Tyler BoddySpargo <muyleche@gmail.com>
2021-04-19 18:47:45 -0500

    fix(projects): fix injection; use  consistently

--- public/scripts/controllers/projects.js
+++ public/scripts/controllers/projects.js
@@ -1,22 +1,19 @@
-define(['scripts/app'],
-  (ngApp) => {
-    function projectController($q, $sce, $timeout, DataService) {
-      const ctrl = this;
-      ctrl.sections = [];
-      ctrl.progress = 0;
-      ctrl.total_progress = 1;
+define(['scripts/app'], (ngApp) => {
+  function projectController($sce, $timeout, DataService) {
+    const ctrl = this;
+    ctrl.sections = [];
 
-      $q.when(DataService.getProjects())
-        .then((data) => {
+    DataService.getProjects()
+      .then((data) => {
+        $timeout(() => {
           ctrl.projects = data;
           for (let x = 0; x < data.length; x++) {
             ctrl.sections.push({ id: `project${x}`, name: data[x].name });
           }
-          ctrl.progress += 1;
           ctrl.projectsReady = true;
         });
+      });
+  };
 
-    };
-
-    ngApp.controller('ProjectsCtrl', ['$q', '$sce', '$timeout', 'DataService', projectController]);
-  });
+  ngApp.controller('ProjectsCtrl', ['$sce', '$timeout', 'DataService', projectController]);
+});
