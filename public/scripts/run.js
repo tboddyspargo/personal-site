bb64681d666ddb7978a8bf193ab4a6d0da003f60
Tyler BoddySpargo <muyleche@gmail.com>
2021-04-19 18:49:51 -0500

    refactor(run): remove switch for scroll delay

--- public/scripts/run.js
+++ public/scripts/run.js
@@ -1,34 +1,24 @@
-define(['scripts/app'],
-  function (ngApp, FirebaseApp) {
-    // initiate app and handle route changes that also scroll to location.
-    ngApp.run(['$rootScope', '$location', '$anchorScroll', '$timeout', function ($rootScope, $location, $anchorScroll, $timeout) {
-      $anchorScroll.yOffset = 60; // always scroll by 50 extra pixels
+define(['scripts/app'], (ngApp) => {
+  // initiate app and handle route changes that also scroll to location.
+  ngApp.run(['$rootScope', '$location', '$anchorScroll', '$timeout', function ($rootScope, $location, $anchorScroll, $timeout) {
+    $anchorScroll.yOffset = 60; // always scroll by 50 extra pixels
 
-      let previousAnchor = '';
-      $rootScope.scrollTo = (anchor = $location.hash(), delay = 0, trigger = 'unknown') => {
-        $timeout(() => {
-          if (anchor != previousAnchor) {
-            $anchorScroll(anchor);
-            previousAnchor = anchor;
-          }
-        }, delay, false);
-      };
-
-      function locationChangeHandler(event) {
-        const currentHash = $location.hash();
-        let delay;
-        switch (event.targetScope.name) {
-          case 'about':
-            delay = 200;
-            break;
-          case 'projects':
-            delay = 200;
-            break;
+    let previousAnchor = '';
+    $rootScope.scrollTo = (anchor = $location.hash(), delay = 0, trigger = 'unknown') => {
+      $timeout(() => {
+        if (anchor != previousAnchor) {
+          $anchorScroll(anchor);
+          previousAnchor = anchor;
         }
-        $rootScope.scrollTo(currentHash, delay, event.name);
-      }
+      }, delay, false);
+    };
 
-      $rootScope.$on('$viewContentLoaded', locationChangeHandler);
+    function locationChangeHandler(event) {
+      const currentHash = $location.hash();
+      $rootScope.scrollTo(currentHash, 200, event.name);
+    }
 
-    }]);
-  });
+    $rootScope.$on('$viewContentLoaded', locationChangeHandler);
+
+  }]);
+});
